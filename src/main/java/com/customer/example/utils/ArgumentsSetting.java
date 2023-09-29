d4508d803d75296f82ae486c0dab397ef5cc0fd5
Danil-Prog <dany.gavrik@mail.ru>
2023-09-30 00:09:17 +0700

    Added class for parse arguments cli

---
+++ src/main/java/com/customer/example/utils/ArgumentsSetting.java
@@ -0,0 +1,46 @@
+package com.customer.example.utils;
+
+import com.customer.example.entity.ResultType;
+import com.customer.example.exception.ParseArgumentsException;
+
+import java.io.File;
+import java.util.Arrays;
+import java.util.Optional;
+
+public class ArgumentsSetting {
+
+    private ResultType typeOperation;
+    private File input;
+    private File output;
+
+    public ArgumentsSetting() {
+    }
+
+    public void parseArguments(String[] args) {
+        if (args.length < 3) {
+            throw new ParseArgumentsException("Error: 3 arguments are required for processing [operation type, path to input and output files]");
+        }
+
+        Optional<ResultType> resultType = Arrays.stream(args).filter(s -> s.equals("search") || s.equals("stat"))
+                .map(s -> s.equals("search") ? ResultType.search : ResultType.stat).findFirst();
+
+        Optional<String> filePathInput = Arrays.stream(args).filter(s -> s.contains("json")).reduce((s, s2) -> s);
+        Optional<String> filePathOutput = Arrays.stream(args).filter(s -> s.contains("json")).reduce((s, s2) -> s2);
+
+        resultType.ifPresent(resultType1 -> typeOperation = resultType1);
+        filePathInput.ifPresent(path -> input = FileUtil.getFile(path));
+        filePathOutput.ifPresent(path -> output = FileUtil.getFile(path));
+    }
+
+    public ResultType getTypeOperation() {
+        return typeOperation;
+    }
+
+    public File getInput() {
+        return input;
+    }
+
+    public File getOutput() {
+        return output;
+    }
+}
