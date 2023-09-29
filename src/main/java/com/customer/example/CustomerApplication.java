e24faa3c96009ce868a4d0d78b1a44f0c8174872
Danil-Prog <dany.gavrik@mail.ru>
2023-09-29 21:39:27 +0700

    Initial project

---
+++ src/main/java/com/customer/example/CustomerApplication.java
@@ -0,0 +1,13 @@
+package com.customer.example;
+
+/**
+ * Hello world!
+ *
+ */
+public class CustomerApplication
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
