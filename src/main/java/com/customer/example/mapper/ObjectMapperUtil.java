1e7c3565a2ac10ec6f20c9ff5fe82462997b9d0b
Danil-Prog <dany.gavrik@mail.ru>
2023-10-01 16:30:10 +0700

    Added parse json to object

---
+++ src/main/java/com/customer/example/mapper/ObjectMapperUtil.java
@@ -0,0 +1,40 @@
+package com.customer.example.mapper;
+
+import com.customer.example.entity.*;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.io.File;
+import java.io.IOException;
+
+/*
+ * Map json to object
+ * */
+public class ObjectMapperUtil {
+
+    public static Search mapToSearch(File file) throws IOException {
+        ObjectMapper objectMapper = new ObjectMapper();
+
+        return objectMapper.readValue(file, Search.class);
+    }
+
+    public static Stat mapToStat(File file) throws IOException {
+        ObjectMapper objectMapper = new ObjectMapper();
+
+        return objectMapper.readValue(file, Stat.class);
+    }
+
+    public static void mapResultToJson(File file, Result result) throws IOException {
+        ObjectMapper objectMapper = new ObjectMapper();
+
+        if (result instanceof ErrorResultImpl) {
+            ErrorResultImpl errors = (ErrorResultImpl) result;
+            objectMapper.writeValue(file, errors);
+        } else if (result instanceof SearchResultImpl) {
+            SearchResultImpl search = (SearchResultImpl) result;
+            objectMapper.writeValue(file, search);
+        } else if (result instanceof StatResultImpl) {
+            StatResultImpl stat = (StatResultImpl) result;
+            objectMapper.writeValue(file, stat);
+        }
+    }
+}
