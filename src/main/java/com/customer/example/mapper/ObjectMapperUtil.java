1cb02252dc3f0d06d98ce0c9f9d4ecbb300d43d8
Danil-Prog <dany.gavrik@mail.ru>
2023-10-04 00:49:45 +0700

    Added method for write result to json, and method json to object Customer

--- src/main/java/com/customer/example/mapper/ObjectMapperUtil.java
+++ src/main/java/com/customer/example/mapper/ObjectMapperUtil.java
@@ -1,8 +1,13 @@
 package com.customer.example.mapper;
 
 import com.customer.example.entity.*;
+import com.customer.example.entity.db.Customer;
 import com.customer.example.entity.db.Product;
+import com.customer.example.exception.JsonParseException;
+import com.customer.example.exception.StatParseException;
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.exc.InvalidFormatException;
 
 import java.io.File;
 import java.io.IOException;
@@ -21,7 +26,11 @@ public class ObjectMapperUtil {
     public static Stat mapToStat(File file) throws IOException {
         ObjectMapper objectMapper = new ObjectMapper();
 
-        return objectMapper.readValue(file, Stat.class);
+        try {
+            return objectMapper.readValue(file, Stat.class);
+        } catch (InvalidFormatException ex) {
+            throw new StatParseException();
+        }
     }
 
     public static Product[] mapToProduct(String json) throws IOException {
@@ -30,18 +39,37 @@ public class ObjectMapperUtil {
         return objectMapper.readValue(json, Product[].class);
     }
 
-    public static void mapResultToJson(File file, Response response) throws IOException {
+    // If list products in user is empty return null
+    public static Customer mapToCustomerPurchases(String json) {
         ObjectMapper objectMapper = new ObjectMapper();
 
-        if (response instanceof ErrorResponseImpl) {
-            ErrorResponseImpl errors = (ErrorResponseImpl) response;
-            objectMapper.writeValue(file, errors);
-        } else if (response instanceof SearchResponseImpl) {
-            SearchResponseImpl search = (SearchResponseImpl) response;
-            objectMapper.writeValue(file, search);
-        } else if (response instanceof StatResponseImpl) {
-            StatResponseImpl stat = (StatResponseImpl) response;
-            objectMapper.writeValue(file, stat);
+        try {
+            return objectMapper.readValue(json, Customer.class);
+        } catch (JsonProcessingException e) {
+            throw new JsonParseException(e.getMessage());
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static void mapResultToJson(File file, Response response) {
+        ObjectMapper objectMapper = new ObjectMapper();
+
+        try {
+            if (response instanceof ErrorResponseImpl) {
+                ErrorResponseImpl errors = (ErrorResponseImpl) response;
+
+                objectMapper.writeValue(file, errors);
+
+            } else if (response instanceof SearchResponseImpl) {
+                SearchResponseImpl search = (SearchResponseImpl) response;
+                objectMapper.writeValue(file, search);
+            } else if (response instanceof StatResponseImpl) {
+                StatResponseImpl stat = (StatResponseImpl) response;
+                objectMapper.writeValue(file, stat);
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
         }
     }
 }
