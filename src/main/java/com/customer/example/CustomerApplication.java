3a10eb29d84daac23e04997bc7529666f7d396d8
Danil-Prog <dany.gavrik@mail.ru>
2023-10-04 00:51:35 +0700

    update class name

--- src/main/java/com/customer/example/CustomerApplication.java
+++ src/main/java/com/customer/example/CustomerApplication.java
@@ -5,31 +5,31 @@ import com.customer.example.entity.Response;
 import com.customer.example.entity.Search;
 import com.customer.example.entity.Stat;
 import com.customer.example.mapper.ObjectMapperUtil;
-import com.customer.example.utils.ArgumentsSetting;
+import com.customer.example.utils.ArgumentsSettingManager;
 
 import java.io.IOException;
 
 public class CustomerApplication {
     public static void main(String[] args) throws IOException {
-        ArgumentsSetting argumentsSetting = new ArgumentsSetting();
-        argumentsSetting.parseArguments(args);
+        ArgumentsSettingManager argumentsSettingManager = ArgumentsSettingManager.getInstance();
+        argumentsSettingManager.parseArguments(args);
 
         CustomerController customerController = new CustomerController();
         Response response = null;
 
-        switch (argumentsSetting.getTypeOperation()) {
+        switch (argumentsSettingManager.getTypeOperation()) {
             case search:
-                Search search = ObjectMapperUtil.mapToSearch(argumentsSetting.getInput());
+                Search search = ObjectMapperUtil.mapToSearch(argumentsSettingManager.getInput());
                 response = customerController.getCustomersByCriterias(search);
                 break;
             case stat:
-                Stat stat = ObjectMapperUtil.mapToStat(argumentsSetting.getInput());
+                Stat stat = ObjectMapperUtil.mapToStat(argumentsSettingManager.getInput());
                 response = customerController.getCustomerStats(stat);
                 break;
         }
 
         if (response != null) {
-            ObjectMapperUtil.mapResultToJson(argumentsSetting.getOutput(), response);
+            ObjectMapperUtil.mapResultToJson(argumentsSettingManager.getOutput(), response);
         }
     }
 }
