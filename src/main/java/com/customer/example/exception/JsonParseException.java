118b9b570208916331d918ab304ca05451b15cca
Danil-Prog <dany.gavrik@mail.ru>
2023-10-04 00:50:06 +0700

    added write errors to file

--- src/main/java/com/customer/example/exception/JsonParseException.java
+++ src/main/java/com/customer/example/exception/JsonParseException.java
@@ -1,8 +1,19 @@
 package com.customer.example.exception;
 
+import com.customer.example.entity.ErrorResponseImpl;
+import com.customer.example.mapper.ObjectMapperUtil;
+import com.customer.example.utils.ArgumentsSettingManager;
+
+import java.io.File;
+
 public class JsonParseException extends RuntimeException {
 
     public JsonParseException(String message) {
-        super(message);
+        ErrorResponseImpl errorResponse = new ErrorResponseImpl();
+        errorResponse.setMessage(message);
+
+        File output = ArgumentsSettingManager.getInstance().getOutput();
+
+        ObjectMapperUtil.mapResultToJson(output, errorResponse);
     }
 }
