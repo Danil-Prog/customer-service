06b7b0a84ba001d9862aeba85880b5b9a3fab694
Danil-Prog <dany.gavrik@mail.ru>
2023-10-04 00:47:34 +0700

    added default constructor and getter

--- src/main/java/com/customer/example/entity/Stat.java
+++ src/main/java/com/customer/example/entity/Stat.java
@@ -5,4 +5,15 @@ import java.util.Date;
 public class Stat {
     private Date startDate;
     private Date endDate;
+
+    public Stat() {
+    }
+
+    public Date getStartDate() {
+        return startDate;
+    }
+
+    public Date getEndDate() {
+        return endDate;
+    }
 }
