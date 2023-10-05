2a5c0825574a95519651d4aa2119e38f826ab0b5
Danil-Prog <dany.gavrik@mail.ru>
2023-10-06 02:20:46 +0700

    added getting a list of employees and purchases for the period

--- src/main/java/com/customer/example/service/impl/CustomerServiceImpl.java
+++ src/main/java/com/customer/example/service/impl/CustomerServiceImpl.java
@@ -1,21 +1,21 @@
 package com.customer.example.service.impl;
 
 import com.customer.example.dto.CustomerDto;
+import com.customer.example.dto.CustomerStatDto;
 import com.customer.example.entity.*;
 import com.customer.example.entity.db.Customer;
 import com.customer.example.entity.db.Product;
 import com.customer.example.exception.StatParseException;
 import com.customer.example.mapper.CustomerMapper;
+import com.customer.example.mapper.CustomerStatMapper;
 import com.customer.example.repository.CustomerRepository;
 import com.customer.example.service.CustomerService;
 
-import java.text.DateFormat;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
 import java.util.Comparator;
+import java.util.Date;
 import java.util.HashSet;
-import java.util.List;
 import java.util.Set;
+import java.util.concurrent.TimeUnit;
 import java.util.stream.Collectors;
 
 public class CustomerServiceImpl implements CustomerService {
@@ -111,24 +111,41 @@ public class CustomerServiceImpl implements CustomerService {
 
     @Override
     public Response getCustomersStats(Stat stat) {
-        List<Customer> customers = this.customerRepository.getCustomerPurchasesByRangeDates(stat);
-        System.out.println(this.isValidDateFormat(stat));
-        return null;
+        this.isValidDateFormat(stat);
+        int days = (int) this.getTotalDays(stat.getStartDate(), stat.getEndDate());
+
+        StatResponseImpl response = new StatResponseImpl();
+
+        Set<CustomerStatDto> customers = this.customerRepository.getCustomerPurchasesByRangeDates(stat)
+                .stream()
+                .filter(customer -> !customer.getPurchases().isEmpty())
+                .map(CustomerStatMapper::customerStatDto).collect(Collectors.toSet());
+
+        int countCustomers = customers.size();
+        int sumPurchases = customers.stream()
+                .map(CustomerStatDto::getTotalExpenses).mapToInt(Integer::intValue).sum();
+
+        int avg = 0;
+        if (sumPurchases != 0) {
+            avg = sumPurchases / countCustomers;
+        }
+        response.setCustomers(customers);
+        response.setTotalDays(days);
+        response.setTotalExpenses(sumPurchases);
+        response.setAvgExpenses(avg);
+
+        return response;
     }
 
     // Check date format
-    public boolean isValidDateFormat(Stat stat) {
-        DateFormat format = new SimpleDateFormat("yyyy-MM-dd");
-        format.setLenient(true);
+    public void isValidDateFormat(Stat stat) {
+        if (stat.getStartDate().after(stat.getEndDate()))
+            throw new StatParseException("Начальная дата не может быть больше конечной.");
+    }
 
-        try {
-            System.out.println();
-            format.parse(stat.getStartDate().toString());
-            format.parse(stat.getEndDate().toString());
-        }catch (ParseException ex) {
-            throw new StatParseException("Неверный формат даты");
-        }
-
-        return true;
+    // We get the number of days between periods
+    public long getTotalDays(Date startDate, Date endDate) {
+        long diff = endDate.getTime() - startDate.getTime();
+        return TimeUnit.DAYS.convert(diff, TimeUnit.MILLISECONDS);
     }
 }
