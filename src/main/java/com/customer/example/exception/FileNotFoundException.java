598e681d1d5a0b735f9bdf3747cfd160d796a8df
Danil-Prog <dany.gavrik@mail.ru>
2023-10-06 02:22:08 +0700

    if the write file is not found, we create an error file and inform the user

--- src/main/java/com/customer/example/exception/FileNotFoundException.java
+++ src/main/java/com/customer/example/exception/FileNotFoundException.java
@@ -1,8 +1,18 @@
 package com.customer.example.exception;
 
+import com.customer.example.entity.ErrorResponseImpl;
+import com.customer.example.mapper.ObjectMapperUtil;
+
+import java.io.File;
+
 public class FileNotFoundException extends RuntimeException {
 
     public FileNotFoundException(String message) {
-        super(message);
+        ErrorResponseImpl errorResponse = new ErrorResponseImpl();
+        errorResponse.setMessage(message);
+
+        File output = new File("error.json");
+
+        ObjectMapperUtil.mapResultToJson(output, errorResponse);
     }
 }
