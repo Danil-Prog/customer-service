d042cfe32aaabe043475d4742963be5723a0e558
Гаврилов Даниил Геннадьевич <danygavrik05@gmail.com>
2023-10-05 10:42:47 +0700

    errors write to file

--- src/main/java/com/customer/example/exception/SqlException.java
+++ src/main/java/com/customer/example/exception/SqlException.java
@@ -1,8 +1,19 @@
 package com.customer.example.exception;
 
+import com.customer.example.entity.ErrorResponseImpl;
+import com.customer.example.mapper.ObjectMapperUtil;
+import com.customer.example.utils.ArgumentsSettingManager;
+
+import java.io.File;
+
 public class SqlException extends RuntimeException {
 
     public SqlException(String message) {
-        super(message);
+        ErrorResponseImpl errorResponse = new ErrorResponseImpl();
+        errorResponse.setMessage("Возникли ошибки при выполнение sql запроса, проверьте правильность данных в бд. Подробнее: " + message);
+
+        File output = ArgumentsSettingManager.getInstance().getOutput();
+
+        ObjectMapperUtil.mapResultToJson(output, errorResponse);
     }
 }
